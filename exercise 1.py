9f97d26d8fbc19ab60cb710ebad47e071abde520
bishu007 <biswanathbandaki@gmail.com>
2018-10-05 12:29:41 +0530

    new file

---
+++ exercise 1.py
@@ -0,0 +1,7 @@
+name = input("What is your name:")
+age = int(input("How old are you:"))
+year = int(input("Enter the current year:"))
+year1 = str((year-age)+100)
+print(name + " your are 100 years old in " + year1)
+
+input("Press enter to exit:")
